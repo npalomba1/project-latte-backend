fd53673db3510e5beda5a0db35d1ab016a00ce66
Nicholas Palomba <nicholaspalomba16@gmail.com>
2022-06-14 21:37:23 -0400

    added Type model details

--- models/Type.models.js
+++ models/Type.models.js
@@ -1,7 +1,19 @@
 const { Schema, model} = require("mongoose");
 
 const typeSchema = new Schema( {
-    
+    title: {
+        type: String,
+        required: true
+    },
+    hotOrIced: [{
+        type: String,
+        enum: ["Coffee", "Espresso", "Coffee and Espresso"]
+    }],
+    hasMik: [{
+        type: String,
+        enum: ["Milk", "No Milk", "Steamed Milk"]
+    }]
+
 })
 
 const Type = model('Type', typeSchema);
